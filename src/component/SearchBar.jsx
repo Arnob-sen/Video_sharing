7d09ecf6a072ed2cd2c6783264e0b64f7312a6fe
Arnob-sen <arnobsen16.2@gmail.com>
2023-09-16 00:36:09 +0600

    ok_fix_the_bug

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -2,43 +2,28 @@ import { useState } from "react";
 import { Paper } from "@mui/material";
 import {TextField} from "@mui/material";
 
-const SearchBar=({onSubmit})=>
-{
-    const [search,setSearch]=useState('');
-    
-    const handleChange=(event)=>  setSearch(event.target.value);
-    const onKeyPress=(event)=>
-    {
-        if(event.Key==="Enter")
-        {
-            onSubmit(search)
-        }
-
+const SearchBar =  ({ onSubmit }) => {
+    const [search, setSearch] = useState("");
+  
+    const handleChange = (event) => setSearch(event.target.value);
+  
+    const onKeyPress = (event) => {
+      if (event.key === "Enter") {
+        onSubmit(search);
+      }
     }
-
-    
-      
-
-    
-   
-
-    return(
-        <Paper elevation={6} sx={{p:2,width:"100%",display:'flex',justifyContent:'center'}} style={{padding:'25px'}}
-        >
-            <TextField
-            fullWidth
-            label='Search...'
-            value={search}
-            onChange={handleChange}
-            onKeyPress={onKeyPress}
-
-
-
-            />
-
-
-        </Paper>
-    )
-    
-}
-export default SearchBar;
+  
+    return (
+      <Paper elevation={6} style={{ padding: "25px" }}>
+        <TextField
+          fullWidth
+          label="Search..."
+          value={search}
+          onChange={handleChange}
+          onKeyPress={onKeyPress}
+        />
+      </Paper>
+    );
+  }
+  
+  export default SearchBar;
